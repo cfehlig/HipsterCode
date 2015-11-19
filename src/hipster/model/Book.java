f85e9a522652a0410d87e764601e3654cb83f107
cfehlig <cassandrafehlig10@gmail.com>
2015-11-19 08:01:03 -0700

    Created Book.java

--- src/hipster/model/Book.java
+++ src/hipster/model/Book.java
@@ -2,5 +2,82 @@ package hipster.model;
 
 public class Book
 {
+	private int pageCount;
+	private String author;
+	private String subject;
+	private String title;
+	private double price;
+	
+	public Book()
+	{
+		this.pageCount = 0;
+		this.author = "";
+		this.subject = "";
+		this.title = "";
+		this.price = -0.00;
+	}
+	
+	public Book(int pageCount, String author, String subject, String title, double price)
+	{
+		this.pageCount = pageCount;
+		this.author = author;
+		this.subject =subject;
+		this.title = title;
+		this.price = price;
+	}
+
+	public int getPageCount()
+	{
+		return pageCount;
+	}
+
+	public String getAuthor()
+	{
+		return author;
+	}
+
+	public String getSubject()
+	{
+		return subject;
+	}
+
+	public String getTitle()
+	{
+		return title;
+	}
+
+	public double getPrice()
+	{
+		return price;
+	}
+
+	public void setPageCount(int pageCount)
+	{
+		this.pageCount = pageCount;
+	}
+
+	public void setAuthor(String author)
+	{
+		this.author = author;
+	}
+
+	public void setSubject(String subject)
+	{
+		this.subject = subject;
+	}
+
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
+
+	public void setPrice(double price)
+	{
+		this.price = price;
+	}
+	
+	
+	
+	
 
 }
